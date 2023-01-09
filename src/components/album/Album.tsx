566353d33e16c86f81dc2092c907a336e5c85d32
Amin <amin.frontend@gmail.com>
2023-01-09 21:15:20 +0330

    add show icon on album hover

--- src/components/album/Album.tsx
+++ src/components/album/Album.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { AlbumData } from "../../utils/types";
+import Show from "../../assets/images/show.svg";
 
 import styles from "./album.module.scss";
 
@@ -12,11 +13,34 @@ interface Props {
 }
 
 const Album: React.FC<Props> = ({ data }) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  function handleMouseEnter() {
+    setIsHovered(true);
+  }
+
+  function handleMouseLeave() {
+    setIsHovered(false);
+  }
+
   const { title, id } = data;
   return (
     <Link to={`/${id}`}>
-      <div className={styles["album-box"]}>
-        <img className={styles.cover} src={IMG_SRC} alt={title} />
+      <div
+        className={styles["album-box"]}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className={styles["image-box"]}>
+          <img className={styles.cover} src={IMG_SRC} alt={title} />
+
+          <div
+            className={`${styles["icon-box"]} ${isHovered && styles.hovered}`}
+          >
+            <img src={Show} alt="show" />
+          </div>
+        </div>
+
         <h2 className={styles.title}>{title}</h2>
       </div>
     </Link>
