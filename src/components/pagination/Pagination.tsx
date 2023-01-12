0470ad57f0728fccae1ce13697a70bbbf7b4ae49
Amin <amin.frontend@gmail.com>
2023-01-12 16:33:15 +0330

    make photo list header responsive

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -32,6 +32,7 @@ const Pagination: React.FC = () => {
           color="primary"
           onChange={handleChange}
           page={page}
+          size="small"
         />
       </Stack>
     </div>
